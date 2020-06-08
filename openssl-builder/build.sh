4535012d6b0d8f2df279f406a50e214670b50944
sashaCher <sashac@cyberark.com>
2020-06-08 22:29:56 +0300

    openssl builder - split openssl build commad to three: install dependencies, build fips module, build openssl - build.sh builds image if it does not exists or on demand - push.sh pushes image if it does not exist or on demand

--- openssl-builder/build.sh
+++ openssl-builder/build.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-docker build -t registry2.itci.conjur.net/openssl-builder openssl-builder
+
+cd "$(dirname "$0")"
+
+OPENSSL_VER=1.0.2u
+OPENSSL_FIPS_VER=2.0.16
+
+docker pull registry.tld/openssl-builder:"$OPENSSL_VER-fips-$OPENSSL_FIPS_VER"
+
+if [ $? -ne 0 ] || "$1"; then
+  docker build -t openssl-builder:"$OPENSSL_VER-fips-$OPENSSL_FIPS_VER" \
+    --build-arg OPENSSL_VER="$OPENSSL_VER" \
+    --build-arg OPENSSL_FIPS_VER="$OPENSSL_FIPS_VER" \
+    .
+fi
