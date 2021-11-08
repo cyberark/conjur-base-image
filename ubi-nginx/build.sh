1fd8681b02eba42d7dfce7774ddd58f8137704d3
Guy Giat <49121537+guygiat@users.noreply.github.com>
2021-11-08 12:06:58 +0200

    Update build.sh

--- ubi-nginx/build.sh
+++ ubi-nginx/build.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")"
 
 # Needs to be kept as close as possible to appliance's NGINX version.
-NGINX_VERSION=1.14
+NGINX_VERSION=1.20.1
 
 docker build -t ubi-nginx:latest \
   --build-arg NGINX_VERSION="${NGINX_VERSION}" .
