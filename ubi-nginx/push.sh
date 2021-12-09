335cb3e6600d2bef7aa4ef9dedc6fb20e4b64796
Matthew Brace <matthew.brace@cyberark.com>
2021-12-09 15:27:51 -0600

    Update push scripts to support correct version and destinations with autopublish

--- ubi-nginx/push.sh
+++ ubi-nginx/push.sh
@@ -8,24 +8,19 @@ NGINX_VERSION=1.20
 LOCAL_IMAGE="ubi-nginx:latest"
 IMAGE="conjur-nginx"
 REDHAT_IMAGE="scan.connect.redhat.com/ospid-9a3dab9b-64c4-4384-882c-80f26ce98607/${IMAGE}"
+REGISTRY="$(normalize_repo_name "$1")"
+TAG=$(<VERSION)
 
-if [[ -z "${TAG_NAME:-}" ]]; then
-  # This script is NOT being run as part of a tag-triggered build
-  # It will publish only to the internal registry on this run
-  TAG="$(git rev-parse --short HEAD | tr -d '\n')"
-  REGISTRY="$(normalize_repo_name $1)"
-
-  tag_and_push "${LOCAL_IMAGE}" "${REGISTRY}${IMAGE}:${NGINX_VERSION}-${TAG}"
-  tag_and_push "${LOCAL_IMAGE}" "${REGISTRY}${IMAGE}:${NGINX_VERSION}"
-else
-  # The script is being run as part of a tag-triggered build
-  # It will only publish to DockerHub on this run
-  TAG="${TAG_NAME//"v"}"
-
+if [[ -z "${REGISTRY:-}" ]]; then
+  # Push to public registry with VERSION
   if summon -f ../secrets.yml bash -c 'docker login scan.connect.redhat.com -u unused -p "${REDHAT_API_KEY}"'; then
     tag_and_push "${LOCAL_IMAGE}" "${REDHAT_IMAGE}:${TAG}"
   else
     echo 'Failed to log in to scan.connect.redhat.com'
     exit 1
   fi
-fi  
+else
+  # Push to internal locations with VERSION and image versions
+  tag_and_push "${LOCAL_IMAGE}" "${REGISTRY}${IMAGE}:${NGINX_VERSION}-${TAG}"
+  tag_and_push "${LOCAL_IMAGE}" "${REGISTRY}${IMAGE}:${NGINX_VERSION}"
+fi
