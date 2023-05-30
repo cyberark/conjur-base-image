95345a90c8e5e39361020420c1f2d93689006a15
cezary tarnowski <cezary.tarnowski@cyberark.com>
2023-05-30 12:55:44 +0200

    CNJR-1666 an ubuntu image with openssl v3 FIPS provider compiled and configured on the os level

--- ubuntu-ruby-fips/build.sh
+++ ubuntu-ruby-fips/build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+set -e
+
 cd "$(dirname "$0")"
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
-UBUNTU_VERSION=20.04
-RUBY_BUILDER_TAG=3.0.6-fips
-PG_BUILDER_TAG=15-15.3-fips
+UBUNTU_VERSION=22.04
+RUBY_FULL_VERSION=3.0.6
+RUBY_MAJOR_VERSION=3.0
+PG_VERSION=10-10.16
 
 docker build -t ubuntu-ruby-fips:latest \
-  --build-arg UBUNTU_VERSION="$UBUNTU_VERSION" \
-  --build-arg OPENSSL_BUILDER_TAG="$(< "${REPO_ROOT}"/openssl-builder/OPENSSL_BUILDER_TAG)" \
-  --build-arg RUBY_BUILDER_TAG="$RUBY_BUILDER_TAG" \
-  --build-arg PG_BUILDER_TAG="$PG_BUILDER_TAG" \
+  --build-arg UBUNTU_VERSION="${UBUNTU_VERSION}" \
+  --build-arg RUBY_FULL_VERSION="${RUBY_FULL_VERSION}" \
+  --build-arg RUBY_MAJOR_VERSION="${RUBY_MAJOR_VERSION}" \
+  --build-arg PG_VERSION="${PG_VERSION}" \
   .
+
+set +e
---
+++ ubuntu-ruby-fips/build_ruby.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
+
+apt-get update
+apt-get install -y build-essential \
+          libreadline-dev \
+          gcc \
+          make \
+          zlib1g-dev \
+          libssl-dev \
+          dpkg-dev \
+          curl
+
+## Compile ruby
+curl "https://cache.ruby-lang.org/pub/ruby/${RUBY_MAJOR_VERSION}/ruby-${RUBY_FULL_VERSION}.tar.gz" --output "ruby-${RUBY_FULL_VERSION}.tar.gz"
+
+echo "$RUBY_SHA256 ruby-$RUBY_FULL_VERSION.tar.gz" | sha256sum -c -
+
+tar -xvf ruby-"${RUBY_FULL_VERSION}".tar.gz
+cd ruby-"${RUBY_FULL_VERSION}"
+./configure --prefix=/usr --with-openssl-dir=/usr/lib/ssl
+make -j4
+make install
+
+set -e
