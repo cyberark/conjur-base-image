0d81d2c77e42d51e3484114df503699eb8432a1c
cezary tarnowski <cezary.tarnowski@cyberark.com>
2023-06-01 15:30:30 +0200

    Centralized version management and improved ruby build

--- ubuntu-ruby-fips/build.sh
+++ ubuntu-ruby-fips/build.sh
@@ -5,27 +5,31 @@ set -e
 cd "$(dirname "$0")"
 
 #REPO_ROOT="$(git rev-parse --show-toplevel)"
-UBUNTU_VERSION=22.04
-RUBY_FULL_VERSION=3.0.6
-RUBY_MAJOR_VERSION=3.0
-PG_VERSION=10-10.16
 
-docker build -t ubuntu-ruby-fips:22.04 \
+set -a
+source ../versions.env
+set +a
+
+docker build -t ubuntu-ruby-fips:"${UBUNTU_VERSION}" \
 	--target=ubuntu-ruby-fips-dev \
 	--pull \
-	--build-arg UBUNTU_VERSION="${UBUNTU_VERSION}" \
-	--build-arg RUBY_FULL_VERSION="${RUBY_FULL_VERSION}" \
-	--build-arg RUBY_MAJOR_VERSION="${RUBY_MAJOR_VERSION}" \
-	--build-arg PG_VERSION="${PG_VERSION}" \
+	--build-arg UBUNTU_VERSION \
+	--build-arg RUBY_FULL_VERSION \
+	--build-arg RUBY_MAJOR_VERSION \
+	--build-arg RUBY_SHA256 \
+	--build-arg PG_VERSION \
+	--build-arg OPEN_SSL_FIPS_PROVIDER_VERSION \
 	.
 
-docker build -t ubuntu-ruby-fips:22.04-slim \
+docker build -t ubuntu-ruby-fips:"${UBUNTU_VERSION}"-slim \
 	--target=ubuntu-ruby-fips-slim \
 	--pull \
-	--build-arg UBUNTU_VERSION="${UBUNTU_VERSION}" \
-	--build-arg RUBY_FULL_VERSION="${RUBY_FULL_VERSION}" \
-	--build-arg RUBY_MAJOR_VERSION="${RUBY_MAJOR_VERSION}" \
-	--build-arg PG_VERSION="${PG_VERSION}" \
+	--build-arg UBUNTU_VERSION \
+	--build-arg RUBY_FULL_VERSION \
+	--build-arg RUBY_MAJOR_VERSION \
+	--build-arg RUBY_SHA256 \
+	--build-arg PG_VERSION \
+	--build-arg OPEN_SSL_FIPS_PROVIDER_VERSION \
 	.
 
 set +e
--- ubuntu-ruby-fips/build_openssl_fips_provider.sh
+++ ubuntu-ruby-fips/build_openssl_fips_provider.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+apt-get update
+apt-get install -y \
+	curl \
+	build-essential \
+	checkinstall \
+	zlib1g-dev
+
 curl "https://www.openssl.org/source/openssl-${OPEN_SSL_FIPS_PROVIDER_VERSION}.tar.gz" --output /tmp/openssl.tar.gz
 
 cd /tmp
